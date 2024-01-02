edef49672f2512d228bcf03026508361d715bec9
gorira <tatsuhiko.shigoto@gmail.com>
2024-01-02 23:25:45 +0900

    Update FastAPI code to use motor for asynchronous MongoDB operations

--- LIKE-FastAPI-ver/main.py
+++ LIKE-FastAPI-ver/main.py
@@ -1,14 +1,17 @@
 from fastapi import FastAPI
 import datetime
-from pymongo import MongoClient
+import motor.motor_asyncio
 
 app = FastAPI()
-client = MongoClient("localhost", 27017)
-db = client["diary"]
+client = motor.motor_asyncio.AsyncIOMotorClient("mongo", 27017, username="root", password="example")
+db = client.get_database("diary")
+collection = db.get_collection("diary")
 
 @app.post("/diary/{day}/{name}/{text}")
-def make_diary(day: str, name: str, text: str, q: str = None):
+async def make_diary(day: str, name: str, text: str, q: str = None):
     post_time = datetime.date.today().isoformat()
+    diary = {"day": day, "name": name, "text": text, "post-time": post_time}
+    await collection.insert_one(diary)
     return {"day": day, "name": name, "text": text, "post-time": post_time, "q": q}
 
 
@@ -17,5 +20,10 @@ def get_diary(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
 @app.get("/diaries")
-def get_diaries():
-    return db["diary"].find_one()
+async def get_diaries():
+    diaries = []
+    cursor = collection.find({})
+    for diary in await cursor.to_list(length=100):
+        diary["_id"] = str(diary["_id"])
+        diaries.append(diary)
+    return {"diaries": diaries}
---
+++ docker-entrypoint-initdb.d/main.js
@@ -0,0 +1,6 @@
+db.createCollection("posts", {
+    "day": "2020-01-01",
+    "name": "name",
+    "text": "text",
+    "post-time": "2020-01-01 00:00:00",
+})
